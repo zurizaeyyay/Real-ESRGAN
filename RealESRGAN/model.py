820fa6380ab1c55e516423a38dbfa035aaf1b069
Zuri Ash Zaey <zuri.ash.dev@outlook.com>
2025-07-09 15:38:38 +1000

    fixed huggingface removed features

--- RealESRGAN/model.py
+++ RealESRGAN/model.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 import time
 import cv2
-from huggingface_hub import hf_hub_url, cached_download
+from huggingface_hub import hf_hub_download
 
 from .rrdbnet_arch import RRDBNet
 from .utils import pad_reflect, split_image_into_overlapping_patches, stich_together, \
@@ -14,15 +14,15 @@ from .progress import ProgressTracker
 
 HF_MODELS = {
     2: dict(
-        repo_id='sberbank-ai/Real-ESRGAN',
+        repo_id='ai-forever/Real-ESRGAN',
         filename='RealESRGAN_x2.pth',
     ),
     4: dict(
-        repo_id='sberbank-ai/Real-ESRGAN',
+        repo_id='ai-forever/Real-ESRGAN',
         filename='RealESRGAN_x4.pth',
     ),
     8: dict(
-        repo_id='sberbank-ai/Real-ESRGAN',
+        repo_id='ai-forever/Real-ESRGAN',
         filename='RealESRGAN_x8.pth',
     ),
 }
@@ -42,10 +42,14 @@ class RealESRGAN:
             assert self.scale in [2,4,8], 'You can download models only with scales: 2, 4, 8'
             config = HF_MODELS[self.scale]
             cache_dir = os.path.dirname(model_path)
-            local_filename = os.path.basename(model_path)
-            config_file_url = hf_hub_url(repo_id=config['repo_id'], filename=config['filename'])
-            cached_download(config_file_url, cache_dir=cache_dir, force_filename=local_filename)
-            print('Weights downloaded to:', os.path.join(cache_dir, local_filename))
+            local_name = os.path.basename(model_path)
+            # Downloads cache and weight data to same folder
+            hf_hub_download(repo_id=config['repo_id'],
+                            filename=config['filename'], 
+                            cache_dir=cache_dir, 
+                            local_dir=cache_dir,
+                            local_dir_use_symlinks=False)
+            print('Weights downloaded to:', os.path.join(cache_dir, local_name))
         
         loadnet = torch.load(model_path)
         if 'params' in loadnet:
